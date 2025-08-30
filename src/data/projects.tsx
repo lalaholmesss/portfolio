9b2351fca350e644b2c52fe7b0dea3d9c5255266
lalaholmesss <lalaholmesart00@gmail.com>
2025-08-31 00:13:20 +0400

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -11,28 +11,28 @@ let projects: Projects[] = [
         id: 1,
         name: 'Lala',
         link: 'some link',
-        img: '/images/lala.png',
+        img: `${import.meta.env.BASE_URL}images/lala.png`,
         highlight: 'hover:bg-green-highlight/60'
     },
     {
         id: 2,
         name: 'Kikis Delivery Service Bento Box',
         link: 'https://sketchfab.com/3d-models/bento-box-fc4b6dd037584e9186b65464d40b1691',
-        img: '/images/kikis cat.png',
+        img: `${import.meta.env.BASE_URL}images/kikis cat.png`,
         highlight: 'hover:bg-yellow-highlight/60'
     },
     {
         id: 3,
         name: 'Glowing Mushrooms',
         link: 'https://sketchfab.com/3d-models/glowing-mushroom-0c1f302ba1664d2f87e6b362a426d059',
-        img: '/images/mushroom.png',
+        img: `${import.meta.env.BASE_URL}images/mushroom.png`,
         highlight: 'hover:bg-pink-highlight/60'
     },
     {
         id: 4,
         name: 'Vending Machine',
         link: 'https://sketchfab.com/3d-models/vending-machine-6f94007cd6824ebcaa22d09e2cca49a0',
-        img: '/images/vending machine.png',
+        img: `${import.meta.env.BASE_URL}images/vending machine.png`,
         highlight: 'hover:bg-violet-highlight/60'
     },
 
