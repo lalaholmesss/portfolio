feeea739daa8576068db2358e8eba7a5cf3584fe
lalaholmesss <lalaholmesart00@gmail.com>
2025-09-01 13:30:06 +0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className='max-w-5xl border-r-2 border-l-2 border-white-10 mx-auto selection:bg-pink-400 selection:text-white'>
       <Navbar />
-      <Header />
-      <Projects />
+      <Routes >
+        <Route path='/' element={<Header />} />
+        <Route path='/projects' element={<Projects/>} />
+      </Routes>
       <Footer />
+
+      
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
+import { Link, useLocation } from "react-router-dom"
+
 export default function Navbar() {
     return(
-        <div className='bg-transparent font-nunito py-5 px-5'>
+        <nav className='bg-transparent font-nunito py-5 px-5'>
             <ul className='space-x-20 text-white-50 text-right flex justify-end'>
-                <li className='cursor-pointer hover:text-white transition duration-300'>Home</li>
-                <li className='cursor-pointer hover:text-white transition duration-300'>Projects</li>
-                <li className='cursor-pointer hover:text-white transition duration-300'>About</li>
+
+                <li className='cursor-pointer hover:text-white transition duration-300'>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li className='cursor-pointer hover:text-white transition duration-300'>
+                    <Link to='/projects'>Projects</Link>
+                </li>
+                <li className='cursor-pointer hover:text-white transition duration-300'>
+                    <Link to='/about'>About</Link>
+                </li>
             </ul>
-        </div>
+        </nav>
     )
 }
